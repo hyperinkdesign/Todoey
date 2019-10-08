e67c71c854b811911cb35346c3dfd94b8e9bb320
Jamie <hyperink@icloud.com>
2019-10-08 14:36:34 +0100

    add new item functionality complete

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    //let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"] // imutable array
+    
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]   //mutable array
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,6 +47,39 @@ class TodoListViewController: UITableViewController {
         }
         
         tableView.deselectRow(at: indexPath, animated: true) // animate the row selection to flash 1 sec grey
+    }
+    
+    
+    //MARK - Add new items
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) { // add the bar button item
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new todo item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            //what will happen once the user clicks the add item button
+            //print("success!")
+            //print(textField.text)
+            self.itemArray.append(textField.text!) // add item fro mthe text field
+            
+            self.tableView.reloadData() // reload the text field to add the item to the array
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "create new item"
+            textField = alertTextField // extending the scope of the alert textField
+            
+//            print(alertTextField.text)
+//            print("now")
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
